ad2f217b640be2568053c12821c892c833973471
GooxCo <107900975+GooxCo@users.noreply.github.com>
2022-06-23 06:06:22 +0700

    Add files via upload

--- router-config/openwrt-imagebuilder/imagebuilder.sh
+++ router-config/openwrt-imagebuilder/imagebuilder.sh
@@ -149,7 +149,7 @@ rebuild_firmware() {
         ath9k-htc-firmware btrfs-progs hostapd hostapd-utils kmod-ath kmod-ath9k kmod-ath9k-common \
         kmod-ath9k-htc kmod-cfg80211 kmod-crypto-acompress kmod-crypto-crc32c kmod-crypto-hash \
         kmod-fs-btrfs kmod-mac80211 wireless-tools wpa-cli wpa-supplicant \
-        libc php7 php7-cgi php7-mod-session php7-mod-ctype php7-mod-fileinfo php7-mod-zip php7-mod-iconv php7-mod-mbstring php7-mod-json \
+        libc php8 php8-cgi php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv php8-mod-mbstring \
         zoneinfo-core zoneinfo-asia nano htop unzip wget wget-ssl libmbedtls tar bash luci-app-mwan3 luci-theme-material \
         git git-http jq openssh-client openssl-util https-dns-proxy luci-app-ttyd ttyd zram-swap luci-app-vnstat2 vnstat2 curl ca-certificates \
         netdata httping coreutils-timeout perl fdisk \
@@ -158,7 +158,7 @@ rebuild_firmware() {
         kmod-usb-net-huawei-cdc-ncm kmod-usb-serial kmod-usb-serial-option kmod-usb-serial-wwan block-mount usb-modeswitch usbutils \
         kmod-usb-net-asix kmod-usb-net-asix-ax88179 kmod-usb-net-dm9601-ether kmod-usb-net-rtl8152 \
         \
-        luci-app-amlogic luci-i18n-amlogic \
+        luci-app-amlogic  \
         "
 
     # Rebuild firmware
