3e2c9d9508701079775a58c638ebff6da3421681
GooxCo <107900975+GooxCo@users.noreply.github.com>
2022-07-23 06:16:08 +0700

    Update imagebuilder.sh

--- router-config/openwrt-imagebuilder/imagebuilder.sh
+++ router-config/openwrt-imagebuilder/imagebuilder.sh
@@ -194,7 +194,7 @@ rebuild_firmware() {
         ath9k-htc-firmware kmod-ath kmod-ath9k kmod-ath9k-common kmod-ath9k-htc \
         kmod-crypto-acompress kmod-crypto-crc32c kmod-crypto-hash \
         kmod-fs-btrfs libc htop libmbedtls ca-certificates openssh-client openssl-util \
-        zram-swap httping netdata coreutils-timeout perl \
+        zram-swap httping netdata coreutils-timeout perl -libustream-wolfssl \
         ${config_list} \
         "
 
