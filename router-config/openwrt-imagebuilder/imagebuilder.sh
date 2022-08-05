9548336ddc689e68a288bf95a54871ef2fe99be9
GooxCo <107900975+GooxCo@users.noreply.github.com>
2022-08-06 05:53:41 +0700

    Update imagebuilder.sh

--- router-config/openwrt-imagebuilder/imagebuilder.sh
+++ router-config/openwrt-imagebuilder/imagebuilder.sh
@@ -197,7 +197,7 @@ rebuild_firmware() {
         kmod-crypto-acompress kmod-crypto-crc32c kmod-crypto-hash \
         kmod-fs-btrfs libc htop libmbedtls ca-certificates openssh-client \
         zram-swap httping netdata coreutils-timeout perl netdata vnstat2 \
-        libc px5g-wolfssl luci-ssl iptables-nft firewall \
+        libc px5g-wolfssl luci-ssl iptables-nft firewall -firewall4 \
         ${config_list} \
         "
 
