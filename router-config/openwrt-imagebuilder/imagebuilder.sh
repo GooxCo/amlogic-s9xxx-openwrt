663dfa404db75a7a798092b14039b87a7dcff514
GooxCo <107900975+GooxCo@users.noreply.github.com>
2022-10-14 05:03:57 +0700

    Add files via upload

--- router-config/openwrt-imagebuilder/imagebuilder.sh
+++ router-config/openwrt-imagebuilder/imagebuilder.sh
@@ -170,14 +170,14 @@ rebuild_firmware() {
         btrfs-progs busybox bzip2 cgi-io chattr comgt comgt-ncm containerd coremark  \
         coreutils coreutils-base64 coreutils-nohup coreutils-truncate curl  \
         dosfstools dumpe2fs e2freefrag e2fsprogs exfat-mkfs  \
-        f2fs-tools f2fsck fdisk getopt git git-http gzip hostapd-utils hostapd-common iconv iw iwinfo jq jshn  \
+        f2fs-tools f2fsck fdisk getopt git git-http gzip hostapd hostapd-utils hostapd-common iconv iw iwinfo jq jshn  \
         kmod-brcmfmac kmod-brcmutil kmod-cfg80211 kmod-mac80211 libjson-script  \
         liblucihttp liblucihttp-lua libnetwork losetup lsattr lsblk lscpu mkf2fs  \
         mount-utils nano openssl-util parted perl-http-date perlbase-file perlbase-getopt  \
         perlbase-time perlbase-unicode perlbase-utf8 pigz ppp ppp-mod-pppoe  \
         pv resize2fs subversion-client subversion-libs tar  \
         tini ttyd tune2fs uclient-fetch uhttpd uhttpd-mod-ubus unzip uqmi usb-modeswitch  \
-        uuidgen wget-ssl wpa-cli wpad-basic wwan xfs-fsck xfs-mkfs xz  \
+        uuidgen wget-ssl wpa-cli wpa-supplicant wwan xfs-fsck xfs-mkfs xz  \
         xz-utils ziptool zoneinfo-asia zoneinfo-core zstd  \
         \
         luci luci-base luci-compat luci-lib-base  \
