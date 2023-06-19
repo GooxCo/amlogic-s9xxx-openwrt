35bcae943d7f0cecf292f8d540a8db481a27eee2
Milo <107900975+JJ-Coffee@users.noreply.github.com>
2023-06-19 16:10:38 +0700

    Update imagebuilder.sh

--- config-openwrt/imagebuilder/imagebuilder.sh
+++ config-openwrt/imagebuilder/imagebuilder.sh
@@ -227,7 +227,7 @@ rebuild_firmware() {
         "
 
     # Rebuild firmware
-    make image PROFILE="Default" PACKAGES="${my_packages}" FILES="files"
+    make image PROFILE="DEVICE_generic" PACKAGES="${my_packages}" FILES="files"
 
     sync && sleep 3
     echo -e "${INFO} [ openwrt/bin/targets/armvirt/64 ] directory status: $(ls bin/targets/*/* -l 2>/dev/null)"
